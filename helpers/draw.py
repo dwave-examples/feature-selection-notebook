9f2aa12120e219aa505e0c7f0587b8b025adcd6d
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-03-16 07:52:29 -0700

    Replace Bokeh in ``draw.plot_bqm()``

--- helpers/draw.py
+++ helpers/draw.py
@@ -16,52 +16,18 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import numpy as np
 import networkx as nx
-import sys
-
-from bokeh.io import show, output_notebook
-from bokeh.models import Plot, Range1d, MultiLine, Circle, Label, LabelSet, ColumnDataSource
-from bokeh.models import WheelZoomTool, ZoomInTool, ZoomOutTool, ResetTool, PanTool
-from bokeh.models.graphs import from_networkx
-
-me = sys.modules[__name__]
-if not hasattr(me, 'bokeh_loaded'):
-    output_notebook()
-    bokeh_loaded = True
 
 def plot_bqm(bqm):
     """Plot binary quadratic model as a labeled graph."""
     g = nx.Graph()
     g.add_nodes_from(bqm.variables)
     g.add_edges_from(bqm.quadratic)
-    plot_size = 400
-    text_size = '16pt'
 
-    graph = from_networkx(g, nx.spring_layout)
-    graph.node_renderer.glyph = Circle(size=35, fill_color='purple', fill_alpha=0.25)
-    graph.edge_renderer.glyph = MultiLine(line_alpha=0.8, line_width=2)
-
-    pos = nx.spring_layout(g)
-    data = {'xpos': [], 'ypos': [], 'label': []}
-    for label, loc in pos.items():
-        data['label'].append(label)
-        data['xpos'].append(loc[0])
-        data['ypos'].append(loc[1])
-    labels = LabelSet(x='xpos', y='ypos', text='label', level='glyph',
-                      source=ColumnDataSource(data), x_offset=-1, y_offset=-1,
-                      text_color="blue", text_font_size='14pt', text_font_style='bold')
-
-    plot = Plot(plot_width=plot_size, plot_height=plot_size, x_range=Range1d(-1.3, 1.3), y_range=Range1d(-1.3, 1.3))
-    plot.title.text = "BQM with {} nodes and {} edges".format(len(bqm), len(bqm.quadratic))
-
-    tools = [WheelZoomTool(), ZoomInTool(), ZoomOutTool(), PanTool(), ResetTool()]
-    plot.add_tools(*tools)
-    plot.toolbar.active_scroll = tools[0]
-
-    plot.renderers.append(graph)
-    plot.add_layout(labels)
-    plot.background_fill_color = "lightyellow"
-
-    show(plot)
+    plt.figure(figsize=(8, 8))
+    ax = plt.gca()
+    ax.set_title(f"BQM with {len(bqm)} nodes and {len(bqm.quadratic)} edges")
+    nx.draw_circular(g, with_labels=True, node_size=3000, node_color="y")
+    plt.show()
 
 def plot_feature_selection(features, selected_features):
     fig = plt.figure(figsize=(6, 6))
